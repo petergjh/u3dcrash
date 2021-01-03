05a5f3cbd3702a0e4951c7655ccfcf85be703414
PeterGao <306739889@qq.com>
2021-01-03 17:00:24 +0800

    PureMVC

---
+++ TestClientMVC/Assets/Scripts/ApplicationFacade.cs
@@ -0,0 +1,44 @@
+﻿/***
+ * 
+ *    Title: "PureMVC" 框架项目
+ *           主题： PureMVC 项目全局控制类
+ *    Description: 
+ *           功能： 
+ *                  
+ *    Date: 2017
+ *    Version: 0.1版本
+ *    Modify Recoder: 
+ *    
+ *   
+ */
+using PureMVC.Patterns.Facade;
+using System;
+using UnityEngine;
+
+namespace TestPureMVC
+{
+    public class ApplicationFacade : Facade
+    {
+
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="goRootNode">UI界面的根结点</param>
+        public ApplicationFacade(GameObject goRootNode)
+        {
+            /* MVC 三层的关联绑定 */
+            //控制层注册（"命令消息"与控制层类的对应关系，建立绑定）
+            //RegisterCommand("Reg_StartDataCommand", typeof(DataCommand));
+            var facade = Facade.GetInstance( () => new Facade() );
+            facade.RegisterCommand("Reg_AddNumberCommand", () => new AddNumberCommand() );
+            facade.RegisterCommand("Reg_SubNumberCommand", () => new SubNumberCommand() );
+
+            //视图层注册
+            RegisterMediator(new DataMediator(goRootNode));
+            //模型层注册
+            RegisterProxy(new DataProxy());
+        }
+
+    }
+}
---
+++ TestClientMVC/Assets/Scripts/View/DataMediator.cs
@@ -0,0 +1,95 @@
+﻿using PureMVC.Interfaces;
+using PureMVC.Patterns.Mediator;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataMediator : Mediator
+{
+    //定义名称
+    public new const string NAME = "DataMediator";
+    //定义两个显示的控件
+    private Text uiTxtLevel;                              //显示“等级”
+    private Button BtnDisplayLevelNum;                  //点击的按钮
+    private Button uiBtnAddLevel;                  //点击的按钮：加
+    private Button uiBtnSubLevel;                  //点击的按钮：减
+
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="goRootNode">界面UI的根结点</param>
+    public DataMediator(GameObject goRootNode):base(NAME,goRootNode)
+    {
+        //确定控件
+        //TxtLevel = goRootNode.transform.Find("Txt_DisplayNum").gameObject.GetComponent<Text>();
+        uiTxtLevel = goRootNode.transform.Find("TxtLevel").gameObject.GetComponent<Text>();
+        //BtnDisplayLevelNum = goRootNode.transform.Find("BtnCount").gameObject.GetComponent<Button>();
+        uiBtnAddLevel = goRootNode.transform.Find("BtnAdd").gameObject.GetComponent<Button>();
+        uiBtnSubLevel = goRootNode.transform.Find("BtnSub").gameObject.GetComponent<Button>();
+        //注册按钮
+        //BtnDisplayLevelNum.onClick.AddListener(OnClickAddingLevelNumber);
+        uiBtnAddLevel.onClick.AddListener(OnClickAddingLevelNumber);
+        uiBtnSubLevel.onClick.AddListener(OnClickSubLevelNumber);
+    }
+
+ 
+
+    //用户点击事件
+    private void OnClickAddingLevelNumber()
+    {
+        //发送“控制层”的命令消息需要在Facade注册
+        SendNotification("Reg_AddNumberCommand");
+    }
+    private void OnClickSubLevelNumber()
+    {
+        SendNotification("Reg_SubNumberCommand");
+    }
+
+    /// <summary>
+    /// 本视图层，允许接收的消息。
+    /// </summary>
+    /// <returns></returns>
+    //public override IList<string> ListNotificationInterests()
+    //{
+    //    IList<string> listResult = new List<string>();
+
+    //    //可以接收的消息（集合）
+    //    listResult.Add("Msg_AddLevel");
+    //    return listResult;
+    //}
+    public override string[] ListNotificationInterests()
+    {
+        IList<string> listResult = new List<string>();
+
+        //可以接收的消息（集合）
+        listResult.Add("Msg_AddLevel");
+        listResult.Add("Msg_SubLevel");
+        return listResult.ToArray();
+    }
+
+    /// <summary>
+    /// 处理所有其他类，发给本类允许处理的消息
+    /// </summary>
+    /// <param name="notification"></param>
+    public override void HandleNotification(INotification notification)
+    {
+        MyData myData = notification.Body as MyData;
+
+        switch (notification.Name)
+        {
+            case "Msg_AddLevel":
+                //把模型层发来的数据，显示给控件。
+                uiTxtLevel.text = myData.Level.ToString();
+                break;
+            case "Msg_SubLevel":
+                uiTxtLevel.text = myData.Level.ToString();
+                break;
+            default:
+                break;
+        }
+    }
+}
