18f28064020cfab7651da466f32b75af3e148edb
PeterGao <306739889@qq.com>
2021-01-20 08:41:20 +0800

    test slider

---
+++ TestBasicFunctions/Assets/Script/TestSlider/TestSlider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSlider : MonoBehaviour
+{
+    float SliderValue = 0;
+    public Text text;
+    void Update()
+    {
+        SliderValue = transform.GetComponentInChildren<Slider>().value;
+        text.text = SliderValue * 100 + "%";
+    }
+}
